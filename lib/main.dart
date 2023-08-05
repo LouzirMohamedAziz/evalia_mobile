30262a5336188d52114def6acdfe818d149eb51a
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2023-08-06 00:25:44 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,379 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// Author: Mohamed Aziz LOUZIR
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Evalia',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blueGrey, brightness: Brightness.light),
+        ),
+        darkTheme:
+            ThemeData(brightness: Brightness.dark), // If we will use theme dark
+        themeMode:
+            ThemeMode.light, // in Here we choose the Theme, dark or light
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [WelcomePage(), RatingsPage(), IndicatorsPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 182, 223, 243),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_rate),
+            label: 'Ratings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rate_review_outlined),
+            label: 'Indicators',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Color.fromARGB(255, 65, 111, 134),
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class AuthenticationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Authentication'),
+        backgroundColor: Color.fromARGB(255, 88, 164, 201),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              // Your logo widget here
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/logo.png'), // Replace with your logo image
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle sign-in logic
+              },
+              child: Text('Sign In'),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Handle sign-up logic
+              },
+              child: Text('Don\'t have an account? Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Column(children: [
+      AppBar(
+          title: Text('Welcome to Evalia'),
+          titleTextStyle:
+              TextStyle(color: Color.fromARGB(255, 30, 44, 52), fontSize: 30),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 114, 169, 196)),
+      Expanded(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ))
+    ]);
+  }
+}
+
+class RatingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cardTextStyle = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 30, 44, 52),
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 100),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color.fromARGB(255, 14, 61, 92), // Border color
+              ),
+              borderRadius:
+                  BorderRadius.circular(8), // Adjust border radius as needed
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Select Actor by:',
+                style: cardTextStyle,
+                semanticsLabel: 'Actors Criteria',
+              ),
+            ),
+          ),
+        ),
+        SearchCriteriaTextField(label: 'Actor Name'),
+        SearchCriteriaTextField(label: 'Sector'),
+        SearchCriteriaTextField(label: 'Sub-sector'),
+        SearchCriteriaTextField(label: 'Country'),
+        SearchCriteriaTextField(label: 'Governorate'),
+        SearchCriteriaTextField(label: 'Delegation'),
+        SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 50,
+              child: ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SearchResultsPage(),
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.loop),
+                  label: Text('Search'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 63, 115, 141),
+                      foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                      ))),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class SearchCriteriaTextField extends StatelessWidget {
+  final String label;
+
+  const SearchCriteriaTextField({required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+}
+
+class IndicatorsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favorites = appState.favorites;
+
+    return ListView.builder(
+      itemCount: favorites.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(favorites[index].asPascalCase),
+        );
+      },
+    );
+  }
+}
+
+// SearchResultsPage
+class SearchResultsPage extends StatefulWidget {
+  @override
+  _SearchResultsPageState createState() => _SearchResultsPageState();
+}
+
+class _SearchResultsPageState extends State<SearchResultsPage> {
+  List<Actor> searchResults = []; // Populate this list with search results
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 162, 209, 233),
+      appBar: AppBar(
+        title: Text('Search Results'),
+        backgroundColor: Color.fromARGB(255, 195, 233, 255),
+      ),
+      body: ListView.builder(
+        itemCount: searchResults.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(searchResults[index].name),
+            // ... Other actor information
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Actor model
+class Actor {
+  final String name;
+
+  Actor(this.name);
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.titleLarge!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asPascalCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
