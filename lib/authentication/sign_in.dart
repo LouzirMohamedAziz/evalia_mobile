e5c00f3958e38974b3075f3d2d4cd6ae8c1a91dd
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2023-08-11 19:26:57 +0100

    adding the non functionnal sign in , sign up page

--- lib/authentication/sign_in.dart
+++ lib/authentication/sign_in.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../main.dart';
+import '../reusable/reusable_widget.dart';
+import 'sign_up.dart';
+
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
@@ -8,10 +12,77 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(decoration: BoxDecoration(
-      gradient: LinearGradient(colors: )
-    )));
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 68, 158, 219),
+              Color.fromARGB(255, 6, 80, 145),
+              Color.fromARGB(255, 4, 39, 58)
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            child: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(
+                        20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+                    child: Column(
+                      children: <Widget>[
+                        Text("Welcome to Evalia",
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 44, 58, 65),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        logoWidget("assets/images/evalia_logo_old.png"),
+                        SizedBox(
+                          height: 80,
+                        ),
+                        textField("Enter your E-mail", Icons.person_outline,
+                            true, _emailTextController),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        textField("Enter your Password", Icons.lock_outline,
+                            true, _passwordTextController),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        signInSignUpButton(context, bool, true, () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MyHomePage()));
+                        }),
+                        signUpOption()
+                      ],
+                    )))));
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account?",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignUp()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
   }
 }
